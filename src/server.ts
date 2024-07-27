3beee27a1bed57beeac16671d345b37ccc6a0777
Matheus Keidy <74063350+matheuskeidygomes@users.noreply.github.com>
2024-07-27 10:15:26 -0300

    update

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,6 @@ const server = express();
 server.use(cors());
 server.use(express.json());
 server.get('/', (req: Request, res: Response) => {
-  res.json({ message: 'Hello World!' });
+  res.json({ message: 'Hello World Update!' });
 });
 server.listen(3000);
